c4005993a39d4c6b665d078494cba13710054395
esteemedtogami <sam.pickell01@gmail.com>
2022-03-13 15:34:12 -0400

    Basic frontend work

---
+++ JS/HelloCrypto.js
@@ -0,0 +1,29 @@
+/*
+    Last Updated March 13, 2022
+    Copyright (C) 2022 Sam Pickell
+    HelloCrypto Coding Challenge - Home page (JavaScript)
+*/
+
+var xValues = [50,60,70,80,90,100,110,120,130,140,150];
+var yValues = [7,8,8,9,9,9,10,11,14,14,15];
+
+new Chart("cryptoChart", {
+  type: "line",
+  data: {
+    labels: xValues,
+    datasets: [{
+      fill: false,
+      lineTension: 0,
+      backgroundColor: "white",
+      borderColor: "black",
+      data: yValues
+    }]
+  },
+  options: {
+    legend: {display: false},
+    backgroundColor: "white",
+    scales: {
+      yAxes: [{ticks: {min: 6, max:16}}],
+    }
+  }
+});
