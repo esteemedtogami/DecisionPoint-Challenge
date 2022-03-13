ef3e51e375f09852acf779e8276cdda8199d1396
esteemedtogami <sam.pickell01@gmail.com>
2022-03-13 18:44:09 -0400

    Working build to be submitted.

--- JS/HelloCrypto.js
+++ JS/HelloCrypto.js
@@ -4,42 +4,110 @@
     HelloCrypto Coding Challenge - Home page (JavaScript)
 */
 
-
-function parseAndDisplayCrypto()
+function parseCrypto(callBack)
 {
+    // Obtain data from csv
+    Papa.parse(cryptoCSV.files[0], {
+	complete: function(results) {
+		callBack(results.data);
+	   }
+    });
+}
+
+function displayCrypto(csvData)
+{
+    // Set the line chart (div) background color
     var canvasDiv = document.getElementById('canvasDiv');
     canvasDiv.style.backgroundColor = 'white';
 
-    Papa.parse(cryptoCSV.files[0], {
-	complete: function(results) {
-		console.log(results);
-	   }
-    });
-
-    // Obtain data from csv
-
     // Use data obtained from CSV and display in a line chart
-    var xValues = [50,60,70,80,90,100,110,120,130,140,150];
-    var yValues = [7,8,8,9,9,9,10,11,14,14,15];
+    const xDates = [];
+    const yBTC = [];
+    const yETH = [];
+    const yLTC = [];
+    const yBCH = [];
 
-    new Chart("cryptoChart", {
+    // Populate datasets
+    for (let i = 1; i < csvData.length - 1; i++)
+    {
+        xDates.push(csvData[i][0]);
+        yBTC.push(csvData[i][1]);
+        yETH.push(csvData[i][2]);
+        yLTC.push(csvData[i][3]);
+        yBCH.push(csvData[i][4]);
+    }
+
+    // Setup the Line chart
+    var cryptoLineChart = new Chart("cryptoChart", {
       type: "line",
       data: {
-        labels: xValues,
+        // Sets the dates on the x axis
+        labels: xDates,
         datasets: [{
+          // Sets the label used in the legend
+          label: "BTC",
           fill: false,
           lineTension: 0,
-          backgroundColor: "white",
+          // Sets line color
           borderColor: "black",
-          data: yValues
+          // Sets dataset
+          data: yBTC
+        },{
+            label: "ETH",
+            fill: false,
+            lineTension: 0,
+            borderColor: "red",
+            data: yETH
+        },{
+            label: "LTC",
+            fill: false,
+            lineTension: 0,
+            borderColor: "green",
+            data: yLTC
+        },{
+            label: "BCH",
+            fill: false,
+            lineTension: 0,
+            borderColor: "blue",
+            data: yBCH
         }]
       },
       options: {
-        legend: {display: false},
-        backgroundColor: "white",
+        // Scaling needed so that values are more visible
+        // Assistance from stack overflow on converting scientific
+        // notation to decimal:
+        // https://stackoverflow.com/questions/40183188/chart-js-not-allowing-y-axis-steps-with-logarithmic-scale
         scales: {
-          yAxes: [{ticks: {min: 6, max:16}}],
+            yAxes: [{
+                type: "logarithmic",
+                ticks: {
+                    min: 0,
+                    max: 14000,
+                    // Converts from scientific notation to decimal
+                    callback: function(value, index, values) {
+                        return Number(value.toString());
+                    }
+                },
+                // Sets the y axis labels
+                afterBuildTicks: function(cryptoLineChart)
+                {
+                    cryptoLineChart.ticks = [];
+                    cryptoLineChart.ticks.push(0);
+                    cryptoLineChart.ticks.push(100);
+                    cryptoLineChart.ticks.push(200);
+                    cryptoLineChart.ticks.push(300);
+                    cryptoLineChart.ticks.push(400);
+                    cryptoLineChart.ticks.push(500);
+                    cryptoLineChart.ticks.push(7000);
+                    cryptoLineChart.ticks.push(9000);
+                    cryptoLineChart.ticks.push(11000);
+                    cryptoLineChart.ticks.push(13000);
+                }
+            }]
+        },
+        // Displays the legend, helping to identify each line
+        legend: {display: true},
+        backgroundColor: "white",
         }
-      }
     });
 }
--- README.txt
+++ README.txt
@@ -1,9 +1,19 @@
 Copyright (C) 2022 Sam Pickell
 
 This is my coding sample for the DecisionPoint interview process. The work
-in this repository, with the exception of Chart.js and Papa Parse, are my
-own. Chart.js and Papa Parse are both licensed under the MIT license, which
-allows for the commercial and private use, distribution, and modification
-of the software. In my case, it is just the use of the software. The min version
-of Papa Parse is included in this repo as a resource, and Chart.js is used
-with an ajax call.
+in this repository, with the exception of Chart.js, Papa Parse, and some
+assistance noted in a comment, are my own. Chart.js and Papa Parse are both
+licensed under the MIT license, which allows for the commercial and private use,
+distribution, and modification of the software. In my case, it is just the use
+of the software. The min version of Papa Parse is included in this repo as a
+resource, and Chart.js is used with an ajax call.
+
+To run, simply extract the "DecisionPoint Challenge" repo (if zipped),
+navigate to the root of the directory, and double click on the
+"HelloCrypto.html" file.
+
+One improvement I would add if I had unlimited time to work on this would be
+to make the code flexible enough to work with any number of different
+currencies. It would require me to get a little more familiar with the
+specifics of Chart.js to be able dynamically add datasets, as for right now,
+the code is set up to handle four datasets.
