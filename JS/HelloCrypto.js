310e5a4fd6c1ef5a9aa745438ce7b547944827ab
esteemedtogami <sam.pickell01@gmail.com>
2022-03-13 16:37:44 -0400

    Can obtain data from a file with Papa Parse. Not linked to chart yet.

--- JS/HelloCrypto.js
+++ JS/HelloCrypto.js
@@ -4,26 +4,42 @@
     HelloCrypto Coding Challenge - Home page (JavaScript)
 */
 
-var xValues = [50,60,70,80,90,100,110,120,130,140,150];
-var yValues = [7,8,8,9,9,9,10,11,14,14,15];
 
-new Chart("cryptoChart", {
-  type: "line",
-  data: {
-    labels: xValues,
-    datasets: [{
-      fill: false,
-      lineTension: 0,
-      backgroundColor: "white",
-      borderColor: "black",
-      data: yValues
-    }]
-  },
-  options: {
-    legend: {display: false},
-    backgroundColor: "white",
-    scales: {
-      yAxes: [{ticks: {min: 6, max:16}}],
-    }
-  }
-});
+function parseAndDisplayCrypto()
+{
+    var canvasDiv = document.getElementById('canvasDiv');
+    canvasDiv.style.backgroundColor = 'white';
+
+    Papa.parse(cryptoCSV.files[0], {
+	complete: function(results) {
+		console.log(results);
+	   }
+    });
+
+    // Obtain data from csv
+
+    // Use data obtained from CSV and display in a line chart
+    var xValues = [50,60,70,80,90,100,110,120,130,140,150];
+    var yValues = [7,8,8,9,9,9,10,11,14,14,15];
+
+    new Chart("cryptoChart", {
+      type: "line",
+      data: {
+        labels: xValues,
+        datasets: [{
+          fill: false,
+          lineTension: 0,
+          backgroundColor: "white",
+          borderColor: "black",
+          data: yValues
+        }]
+      },
+      options: {
+        legend: {display: false},
+        backgroundColor: "white",
+        scales: {
+          yAxes: [{ticks: {min: 6, max:16}}],
+        }
+      }
+    });
+}
--- README.txt
+++ README.txt
@@ -1,1 +1,9 @@
-Initial commit
+Copyright (C) 2022 Sam Pickell
+
+This is my coding sample for the DecisionPoint interview process. The work
+in this repository, with the exception of Chart.js and Papa Parse, are my
+own. Chart.js and Papa Parse are both licensed under the MIT license, which
+allows for the commercial and private use, distribution, and modification
+of the software. In my case, it is just the use of the software. The min version
+of Papa Parse is included in this repo as a resource, and Chart.js is used
+with an ajax call.
